dde48de9ac4cc0ccae3520721b89eca2f2d7af2d
hrishikeshpanhalkar <hrishikeshpanhalkar7@gmail.com>
2022-02-07 10:03:31 +0530

    Added category list

--- components/index.js
+++ components/index.js
@@ -1,7 +1,9 @@
 import CustomButton from './CustomButton';
+import CategoryCard from './CategoryCard';
 import TabIcon from './TabIcon';
 
 export {
     CustomButton,
-    TabIcon
+    TabIcon,
+    CategoryCard
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,58 @@
 import React from 'react';
 import { View,Text,TouchableOpacity, Image, SafeAreaView, TextInput, FlatList } from 'react-native';
-import { FONTS, COLORS, SIZES, icons, dummyData } from '../constants';
+import { FONTS, COLORS, SIZES, icons, dummyData, images } from '../constants';
+import { CategoryCard } from '../components';
 
 
 const Home = ({ navigation }) => {
+    function renderHeader(){
+        return(
+            <View
+                style={{
+                    flexDirection: 'row',
+                    marginHorizontal: SIZES.padding,
+                    alignItems: 'center',
+                    height:80
+                }}
+            >
+                <View
+                    style={{
+                        flex:1
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: COLORS.darkGreen,
+                            ...FONTS.h2
+                        }}
+                    >
+                        Hello Programmers,
+                    </Text>
+                    <Text
+                        style={{
+                            marginTop:3,
+                            color: COLORS.gray,
+                            ...FONTS.body3
+                        }}
+                    >
+                        What you to cook today?
+                    </Text>
+                </View>
+                <TouchableOpacity
+                        onPress={() => console.log("Profile")}
+                    >
+                        <Image
+                            source={images.profile}
+                            style={{
+                                width:40,
+                                height:40,
+                                borderRadius: 20
+                            }}
+                        />
+                    </TouchableOpacity>
+            </View>
+        )
+    }
     return (
         <SafeAreaView
             style={{
@@ -18,14 +67,18 @@ const Home = ({ navigation }) => {
                 showsVerticalScrollIndicator={false}
                 ListHeaderComponent={
                     <View>
-
+                        {renderHeader()}
                     </View>
                 }
                 renderItem={({item}) => {
                     return(
-                        <View>
-                            <Text>{item.name}</Text>
-                        </View>
+                        <CategoryCard
+                            containerStyle={{
+                                marginHorizontal: SIZES.padding
+                            }}
+                            categoryItem={item}
+                            onPress={() => navigation.navigate("Recipe", {recipe: item})}
+                        />
                     )
                 }}
                 ListFooterComponent={
